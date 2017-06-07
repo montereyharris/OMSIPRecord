673dfa9621f58d4b8d6ca0f51a01d73f0858fc61
montereyharris <monterey.harris@magenium.com>
2017-06-07 14:53:14 -0500

    commit

--- New-OmsIPRecord.ps1
+++ New-OmsIPRecord.ps1
@@ -1,9 +1,40 @@
 Function New-OmSIPRecord {
+<#
+.SYNOPSIS
+Short description
 
+.DESCRIPTION
+Long description
+
+.PARAMETER ComputerName
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
 [CmdletBinding()]
 Param(
 
-    ComputerName
+    [Parameter(Mandatory=$true,ParameterSetName ='Default')]
+    [String[]]
+    $HostName,
+
+    [Parameter(Mandatory=$false,ParameterSetName ='Default')]
+    [Switch]
+    $IncludeIPv6,
+
+    [Parameter(Mandatory=$false,ParameterSetName ='Default')]
+    [String]
+    [ValidateSet('WMI','CIM')]
+    $CommuncationProtocol,
+
+    # Parameter help description
+    [Parameter(Mandatory=$false,ParameterSetName ='Default')]
+    [ParameterType]
+    $ParameterName
 
 
 
